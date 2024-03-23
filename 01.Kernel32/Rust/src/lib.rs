93408a0a8ed92834c826551c31cc06d6e194119b
Youngho Cho <dominoyh5@gmail.com>
2024-03-24 00:01:38 +0900

    update: rewrite kPrintString in rust

--- 01.Kernel32/Rust/src/lib.rs
+++ 01.Kernel32/Rust/src/lib.rs
@@ -12,6 +12,46 @@ extern "C" {
     fn sleep_ms(time: i32) -> i32;
 }
 
+#[repr(C)]
+struct Character {
+    charactor: u8,
+    attribute: u8,
+}
+
+fn c_str_len(c_str: *const u8) -> usize {
+    let mut len = 0;
+
+    unsafe {
+        while *c_str.add(len) != 0 {
+            len += 1;
+        }
+    }
+
+    len
+}
+
+fn c_str_to_slice(c_str: *const u8) -> &'static [u8] {
+    let len = c_str_len(c_str);
+
+    unsafe { core::slice::from_raw_parts(c_str, len) }
+}
+
+#[no_mangle]
+pub extern "C" fn kPrintString(x: usize, y: usize, message: *mut u8) {
+    let message = c_str_to_slice(message);
+    let mut pst_screen = 0xB8000 as *mut Character;
+
+    unsafe {
+        pst_screen = pst_screen.add((y * 80) + x);
+    }
+
+    for (i, c) in message.iter().enumerate() {
+        unsafe {
+            (*pst_screen.add(i)).charactor = *c;
+        }
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn kInitializeKernel64Area() -> u8 {
     for i in 0x100000..0x600000 {
--- 01.Kernel32/Source/Main.c
+++ 01.Kernel32/Source/Main.c
@@ -29,13 +29,3 @@ void Main() {
   while (1)
     ;
 }
-
-void kPrintString(int iX, int iY, const char *pcString) {
-  CHARACTER *pstScreen = (CHARACTER *)0xB8000;
-  int i;
-
-  pstScreen += (iY * 80) + iX;
-  for (i = 0; pcString[i] != 0; i++) {
-    pstScreen[i].bCharactor = pcString[i];
-  }
-}
