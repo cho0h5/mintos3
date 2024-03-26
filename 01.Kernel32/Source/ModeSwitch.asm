277fc0f63caa7524f388464b9932eb19a55b32bd
Youngho Cho <dominoyh5@gmail.com>
2024-03-26 20:30:27 +0900

    add: ModeSwitch.asm/h

---
+++ 01.Kernel32/Source/ModeSwitch.asm
@@ -0,0 +1,29 @@
+[BITS 32]
+
+global kSwitchAndExecute64bitKernel
+
+SECTION .text
+
+kSwitchAndExecute64bitKernel:
+  mov eax, cr4
+  or eax, 0x20
+  mov cr4, eax
+
+  mov eax, 0x100000
+  mov cr3, eax
+
+  mov ecx, 0xC0000080
+  rdmsr
+
+  or eax, 0x0100
+
+  wrmsr
+
+  mov eax, cr0
+  or eax, 0xE0000000
+  xor eax, 0x60000000
+  mov cr0, eax
+
+  jmp 0x08:0x200000
+
+  jmp $
