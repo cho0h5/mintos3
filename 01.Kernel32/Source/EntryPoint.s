bafc7959cfb67c2016a300db8aaf1132d8fc16a4
Youngho Cho <dominoyh5@gmail.com>
2024-03-29 19:41:18 +0900

    update: 01 EntryPoint.s (291p)

--- 01.Kernel32/Source/EntryPoint.s
+++ 01.Kernel32/Source/EntryPoint.s
@@ -13,11 +13,11 @@ START:
 
   mov eax, 0x4000003B
   mov cr0, eax
-  jmp dword 0x08: ( PROTECTEDMODE - $$ + 0x10000 )
+  jmp dword 0x18: ( PROTECTEDMODE - $$ + 0x10000 )
 
 [BITS 32]
 PROTECTEDMODE:
-  mov ax, 0x10
+  mov ax, 0x20
   mov ds, ax
   mov es, ax
   mov fs, ax
@@ -32,7 +32,7 @@ PROTECTEDMODE:
   push 0
   call PRINTMESSAGE
   add esp, 12
-  jmp dword 0x08: 0x10200
+  jmp dword 0x18: 0x10200
 
 PRINTMESSAGE:
   push ebp
@@ -86,6 +86,22 @@ GDT:
     db 0x00
     db 0x00
     db 0x00
+
+  IA_32eCODEDESCRIPTOR:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0x9A
+    db 0xAF
+    db 0x00
+  IA_32eDATADESCRIPTOR:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 0x92
+    db 0xAF
+    db 0x00
+
   CODEDESCRIPTOR:
     dw 0xFFFF
     dw 0x0000
